0485886514fa2f7841311a8eb68d8334457c23e7
Felix Arndt <hallo@silsha.me>
2013-09-11 15:52:04 +0200

    add table to preise

--- js/scripts.js
+++ js/scripts.js
@@ -9,8 +9,23 @@ $(document).ready(function () {
                 tr.append("<td class='success'>✔ Verfügbar</td>");
             else   
                 tr.append("<td class='danger'>✘ Nicht verfügbar</td>");
-            tr.append("<td>" + json[i].price + " €</td>");
-            $('table').append(tr);
+            tr.append("<td>" + (json[i].price * json[i].flaschen).toFixed(2) + " €</td>");
+            $('#angebot').append(tr);
         }
     });
+
+    $.getJSON("../products.json",
+    function (json) {
+        var tr;
+        for (var i = 0; i < json.length; i++) {
+            if(json[i].available){
+                tr = $('<tr/>');
+                tr.append("<th>" + json[i].name + "<br><small>(" + (json[i].price * json[i].flaschen).toFixed(2) + " € netto pro Kasten)</small>" + "</th>");
+                tr.append("<td><input type='text' class='form-control' data-name='" + json[i].name + "' data-type='flasche'></td>");
+                tr.append("<td><input type='text' class='form-control' data-name='" + json[i].name + "' data-type='kiste'></td>");
+                $('#preis').append(tr);
+            }
+        }
+    });
+
 });
